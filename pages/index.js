bbac816245c172fa77dcf9007bc236612859b8ab
danobisiw <obisiw1@gmail.com>
2022-10-13 12:39:43 +0000

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,31 @@
-
 import Image from "next/image";
 import Banner from "../components/Banner";
 import MenuList from "../components/MenuList";
 import styles from "../styles/Home.module.css";
-import Search from "../components/search";
+import Search from "../components/Search";
 import axios from "axios";
 import {getMenus} from "../utils/getMenus"
 
 
 export default function Home({menus}) {
 
-  return (
-    <div className={styles.container}>
-     
-      <Banner />
+    return (<div className={
+        styles.container
+    }>
 
-      <MenuList menus={menus}/>
-    </div>
-  );
+        <Banner/>
+
+        <MenuList menus={menus}/>
+    </div>);
 }
 export const getServerSideProps = async () => {
-  const result=await axios.get("http://localhost:3000/api/menu");
-  // console.log(result)
-  return{
-    
-    props:{
-      menus:result.data.menus
-      
+    const result = await axios.get("http://localhost:3000/api/menu");
+    // console.log(result)
+    return {
+
+        props: {
+            menus: result.data.menus
+
+        }
     }
-  }
 }
-
-
-
