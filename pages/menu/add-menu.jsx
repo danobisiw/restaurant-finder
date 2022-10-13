3e954e3c5962b4b80cb2ef438e5d9f30279e94dc
danobisiw <obisiw1@gmail.com>
2022-10-13 13:08:03 +0000

    updates

--- pages/menu/add-menu.jsx
+++ pages/menu/add-menu.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState,} from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import isWindow from "../../utils/isWindow";
